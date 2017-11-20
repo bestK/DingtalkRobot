7abc5fb3114d23044cc63b86fa49fa95fb01d319
ECH2O <zengmeng@hrbeu.edu.cn>
2017-11-20 15:25:21 +0800

    Update DingtalkRobotUtil.java

--- DingtalkRobotUtil.java
+++ DingtalkRobotUtil.java
@@ -88,6 +88,21 @@ public class DingtalkRobotUtil {
         return returnString;
 
     }
+    
+    @Test
+    public void test() {
+        Long begin = System.currentTimeMillis();
+
+        MapFactory mapFactory = new MapFactory();
+        mapFactory.put("msgtype", "markdown")
+                .put("markdown", new MapFactory().put("text", "![马化腾](https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1374801086,2403386933&fm=27&gp=0.jpg)").put("title", "马化腾给您发消息了").getMap())
+                .put("at", new MapFactory().put("atMobiles", new String[]{"1", "2"}).getMap())
+                .put("isAtAll", false);
+        Long end = System.currentTimeMillis();
+
+        post(mapFactory.getMap());
+        System.out.println((end - begin) + "ms");
+    }
 
 
 }
